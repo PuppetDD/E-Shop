510947d47ea5490d0f39d865de771d90388dfd61
PuppetDD <2475630190@qq.com>
2019-07-20 13:39:36 +0800

    E-Shop

--- src/handler/ServletDelete.java
+++ src/handler/ServletDelete.java
@@ -24,9 +24,6 @@ public class ServletDelete extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
-
-
-        
     }
 
     @Override
